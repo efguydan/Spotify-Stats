9d54d343d3ceb0422be53d089671a26be2c1fd9b
EfeDaniel <EfeEjemudaro@gmail.com>
2020-11-10 17:47:08 +0100

    Initial commit

---
+++ app/src/main/java/com/efedaniel/spotifystats/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.efedaniel.spotifystats
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.efedaniel.spotifystats.ui.SpotifyStatsTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SpotifyStatsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SpotifyStatsTheme {
+        Greeting("Android")
+    }
+}
