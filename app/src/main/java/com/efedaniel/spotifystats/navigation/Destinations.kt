662b1504a3c6c9faf6c0efbc94141dc428c58369
EfeDaniel <EfeEjemudaro@gmail.com>
2020-11-15 20:55:03 +0100

    Fix: fix wrong artist constant value on Destinations.kt

--- app/src/main/java/com/efedaniel/spotifystats/navigation/Destinations.kt
+++ app/src/main/java/com/efedaniel/spotifystats/navigation/Destinations.kt
@@ -2,7 +2,7 @@ package com.efedaniel.spotifystats.navigation
 
 object Destinations {
     const val tracks = "tracks"
-    const val artists = "tracks"
+    const val artists = "artists"
     const val activity = "activity"
     const val insights = "insights"
     // TODO Decide whether to change this to settings later
