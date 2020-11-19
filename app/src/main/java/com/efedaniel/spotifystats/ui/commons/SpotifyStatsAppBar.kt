d04367271f67251bfceeebc0bd02936eb0ab529c
EfeDaniel <EfeEjemudaro@gmail.com>
2020-11-19 21:18:17 +0100

    Feature: Add SpotifyStatsAppBar commons composable

---
+++ app/src/main/java/com/efedaniel/spotifystats/ui/commons/SpotifyStatsAppBar.kt
@@ -0,0 +1,19 @@
+package com.efedaniel.spotifystats.ui.commons
+
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpotifyStatsAppBar(
+    modifier: Modifier = Modifier,
+    title: String = ""
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        elevation = 4.dp,
+        modifier = modifier
+    )
+}
--- app/src/main/java/com/efedaniel/spotifystats/ui/screens/activity/ActivityScreen.kt
+++ app/src/main/java/com/efedaniel/spotifystats/ui/screens/activity/ActivityScreen.kt
@@ -1,15 +1,28 @@
 package com.efedaniel.spotifystats.ui.screens.activity
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.efedaniel.spotifystats.R
+import com.efedaniel.spotifystats.ui.commons.SpotifyStatsAppBar
 import com.efedaniel.spotifystats.ui.theme.SpotifyStatsTheme
 import com.efedaniel.spotifystats.utils.Constants
 import dev.chrisbanes.accompanist.coil.CoilImage
@@ -17,28 +30,52 @@ import dev.chrisbanes.accompanist.coil.CoilImage
 @Composable
 fun ActivityScreen() {
     SpotifyStatsTheme {
-        Row {
-            CurrentlyPlayingTrackCard()
+        Scaffold(
+            topBar = { SpotifyStatsAppBar(title = stringResource(R.string.activity)) }
+        ) {
+            ScrollableColumn(
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = "Currently Playing", style = MaterialTheme.typography.h6)
+                Spacer(modifier = Modifier.height(8.dp))
+                CurrentlyPlayingTrackCard()
+            }
         }
     }
 }
 
 @Composable
 fun CurrentlyPlayingTrackCard(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp).fillMaxWidth()) {
-        Card {
-            TrackImageCard()
+    Card(
+        elevation = 8.dp,
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(Modifier.fillMaxWidth()) {
+            TrackImage(modifier = Modifier.padding(8.dp))
+            Column(
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Text(text = "Blinding Lights", modifier = Modifier.padding(top = 6.dp))
+                Text(text = "The Weeknd")
+                Text(text = "After Hours", modifier = Modifier.padding(bottom = 6.dp))
+            }
         }
     }
 }
 
 @Composable
-fun TrackImageCard(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.size(80.dp)) {
-        CoilImage(
-            data = Constants.Misc.randomImage,
-            contentScale = ContentScale.Crop,
-            fadeIn = true
-        )
-    }
+fun TrackImage(modifier: Modifier = Modifier) {
+    CoilImage(
+        data = Constants.Misc.randomImage,
+        contentScale = ContentScale.Crop,
+        fadeIn = true,
+        modifier = modifier.size(100.dp).clip(MaterialTheme.shapes.medium),
+    )
+}
+
+@Preview
+@Composable
+fun PreviewActivityScreen() {
+    ActivityScreen()
 }
