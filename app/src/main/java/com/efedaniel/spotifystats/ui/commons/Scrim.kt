d4ddb8afb379228160290839acdadd4b849ae6f8
EfeDaniel <EfeEjemudaro@gmail.com>
2020-11-22 17:02:30 +0100

    Refactor: Increase Scrim max alpha to 40%

--- app/src/main/java/com/efedaniel/spotifystats/ui/commons/Scrim.kt
+++ app/src/main/java/com/efedaniel/spotifystats/ui/commons/Scrim.kt
@@ -7,7 +7,7 @@ import com.efedaniel.spotifystats.ui.theme.spotifyBlack
 // TODO Confirm if these colors work for both light and dark theme
 val scrim: LinearGradient get() = VerticalGradient(
     colors = listOf(
-        spotifyBlack.copy(alpha = 0.2f),
+        spotifyBlack.copy(alpha = 0.4f),
         spotifyBlack.copy(alpha = 0.0f)
     ),
     startY = 100f,
