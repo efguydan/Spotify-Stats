058fb0d2bdeb3d4c9d593b63b038f042d470bd9a
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-10-11 14:21:17 +0100

    Checked if The main Spotify Application is installed

--- app/src/main/java/com/efedaniel/spotifystats/ui/scene/routing/RoutingActivity.kt
+++ app/src/main/java/com/efedaniel/spotifystats/ui/scene/routing/RoutingActivity.kt
@@ -1,10 +1,15 @@
 package com.efedaniel.spotifystats.ui.scene.routing
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.spotify.sdk.android.auth.app.SpotifyNativeAuthUtil
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -12,12 +17,14 @@ class RoutingActivity : ComponentActivity() {
 
     private val viewModel: RoutingViewModel by viewModels()
     @Inject lateinit var navigator: RoutingNavigator
+    val context: Context = this@RoutingActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         splashScreen.setKeepOnScreenCondition { true }
-        navigateToNextScreen()
+       // navigateToNextScreen()
+        navigateToNextScreenSDK()
     }
 
     private fun navigateToNextScreen() {
@@ -26,5 +33,24 @@ class RoutingActivity : ComponentActivity() {
         } else {
             navigator.navigateToLogin(this)
         }
+
     }
+
+    private fun navigateToNextScreenSDK(){
+
+        if (SpotifyNativeAuthUtil.isSpotifyInstalled(context)) {
+            Timber.tag("ROUTINGACTIVITY").d("${SpotifyNativeAuthUtil.isSpotifyInstalled(context)}")
+
+           var intent = SpotifyNativeAuthUtil.createAuthActivityIntent(this@RoutingActivity)
+            Timber.tag("ROUTINGACTIVITY").d("${intent}")
+
+            println(intent?.data)
+            startActivity(intent)
+        } else {
+
+        }
+
+    }
+
+
 }
