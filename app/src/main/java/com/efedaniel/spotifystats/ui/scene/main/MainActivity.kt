b760cf3430625cb2f62a16dc0c165051fd2c2c61
Ejemudaro Ufuoma Isaac <ejemudaroufuoma@gmail.cpm>
2025-02-01 12:32:20 +0400

    Full screen mode achieve

--- app/src/main/java/com/efedaniel/spotifystats/ui/scene/main/MainActivity.kt
+++ app/src/main/java/com/efedaniel/spotifystats/ui/scene/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.efedaniel.spotifystats.ui.scene.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -32,6 +33,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ProtonTheme {
                 MainScreen()
